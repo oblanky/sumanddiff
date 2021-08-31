b4ca13ef83b3fe78b9a3c8100594aaab1d82e7d8
oblanky <87757828+oblanky@users.noreply.github.com>
2021-08-31 06:28:38 +0100

    Add files via upload

---
+++ sumanddiff.js
@@ -0,0 +1,6 @@
+let firstChild = 10;
+let secondChild =5;
+let sum = firstChild +secondChild;
+let difference = firstChild - secondChild;
+let print = "The sum of ages is " + sum + " and difference of ages is " + difference;
+console.log(print);
